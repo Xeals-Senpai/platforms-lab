b172acc671a6a4b9b710db68bce0297be2b5a4b9
Xeals <xxZeroxxism@yahoo.co.uk>
2026-07-07 12:12:40 +0100

    Move Terraform state to HCP Terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,18 @@
 terraform {
   required_version = ">= 1.8.0"
 
+  cloud {
+    organization = "xeals"
+
+    workspaces {
+      name = "platforms-lab-local"
+    }
+  }
+
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
     }
   }
-
-  backend "local" {}
 }
